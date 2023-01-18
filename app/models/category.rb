da2497237ab9b2c9715ed944041ec4aa3bc44b03
Volodymyr Tyshchenko <vtyshchenko77@gmail.com>
2023-01-18 17:15:10 +0200

    added validates

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ApplicationRecord
     has_many :operations
+
+
+    validates :amount, presence = true, numericality: { greater_than: 0 }
+    validates :odate, presence = true
+    validates :description, presence = true 
+
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,6 @@
 class Operation < ApplicationRecord
     belongs_to :category
+
+    validates :name, presence: true, uniqueness: true 
+    validates :description, presence: true 
 end
