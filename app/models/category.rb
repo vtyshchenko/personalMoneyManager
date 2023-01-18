daf4178ae23bcac97eaaa72eff5e29d28cc34dd2
Volodymyr Tyshchenko <vtyshchenko77@gmail.com>
2023-01-18 18:22:55 +0200

    bugfix syntax

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,8 @@ class Category < ApplicationRecord
     has_many :operations
 
 
-    validates :amount, presence = true, numericality: { greater_than: 0 }
-    validates :odate, presence = true
-    validates :description, presence = true 
+    validates :amount, presence: true, numericality: { greater_than: 0 }
+    validates :odate, presence: true
+    validates :description, presence: true 
 
 end
